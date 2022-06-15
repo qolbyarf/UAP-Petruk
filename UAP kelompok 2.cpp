436a95082a0ece623c77013a143a2e8d6391f9e6
Bagas Muhammad Shaka <89391428+Bagashaka@users.noreply.github.com>
2022-06-15 11:41:00 +0700

    Create UAP kelompok 2.cpp

---
+++ UAP kelompok 2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+#include <stack>
+using namespace std;
+
+string tanaman[]={"1.Kentang", "2.Tomat", 
+                   "3.Wortel", "4.Jagung", 
+                   "5.Cabai", "6.Bawang Merah", 
+                   "7.Kacang Tanah", "8.Terong", 
+                   "9.Singkong", "10.Bayam"};
+                   
+int harga[]={15000,12000,
+             13000,10000,
+             75000,25000,
+             30000,16000,
+             12500,14000};
+             
+stack<int>pangan;
+int total = 0;
+int a;
+
+void push(){
+    cout << "Silahkan Pilih : " ;
+    cin >> a;
+    if(a > 10){
+        cout << "Sayuran tidak tersedia" << endl;
+    }
+    else{
+        pangan.push(a);
+        total = total + harga[a-1];
+    }
+}
